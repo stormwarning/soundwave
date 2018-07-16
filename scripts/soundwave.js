0a4dc012d2e033bbcb1b9cf1d7d342128b4f4ee0
Jeff <rustyangel@gmail.com>
2018-07-16 11:34:34 -0600

    🔨 Add basic script setup

---
+++ scripts/soundwave.js
@@ -0,0 +1,8 @@
+const Sonos = require('sonos').Sonos
+const speaker = new Sonos(process.env.HUBOT_SONOS_HOST)
+
+module.exports = function(robot) {
+    robot.respond(/status/i, function(msg) {
+        return msg.reply(`${speaker.getSpotifyConnectInfo()}`)
+    })
+}
