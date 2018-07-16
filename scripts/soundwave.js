670ec2b3b71e2cf727c193d1bb18b4b2c27eef4f
Jeff <rustyangel@gmail.com>
2018-07-16 13:17:23 -0600

    Stringify json output

--- scripts/soundwave.js
+++ scripts/soundwave.js
@@ -3,6 +3,6 @@ const speaker = new Sonos(process.env.HUBOT_SONOS_HOST)
 
 module.exports = function(robot) {
     robot.respond(/status/i, function(msg) {
-        return msg.reply(`${speaker.getSpotifyConnectInfo()}`)
+        return msg.reply(`${JSON.stringify(speaker.getSpotifyConnectInfo())}`)
     })
 }
