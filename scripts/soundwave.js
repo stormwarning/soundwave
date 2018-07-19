139c162045324c5d2d566a869a0f343f9f0dc3b9
Jeff <rustyangel@gmail.com>
2018-07-19 09:24:32 -0600

    🔨 Add play function

--- scripts/soundwave.js
+++ scripts/soundwave.js
@@ -1,8 +1,25 @@
 const Sonos = require('sonos').Sonos
 const speaker = new Sonos(process.env.HUBOT_SONOS_HOST)
 
-module.exports = function(robot) {
-    robot.respond(/status/i, function(msg) {
+const PLAYLIST = 'spotify:user:rustyangel:playlist:30Lf4GhZN8VIvyvcCFR2BY'
+
+function _log (...args) {
+    console.log('MM-DD hh:mm:ss:iii  '.timestamp, ...args)
+}
+
+module.exports = function (robot) {
+    robot.respond(/status/i, function (msg) {
         return msg.reply(`${JSON.stringify(speaker.getSpotifyConnectInfo())}`)
+    })
+
+    robot.respond(/standup time/i, function (msg) {
+        speaker
+            .play(PLAYLIST)
+            .then((result) => {
+                _log('Started playing', result)
+            })
+            .catch((error) => {
+                _log('An error occurred:', error)
+            })
     })
 }
